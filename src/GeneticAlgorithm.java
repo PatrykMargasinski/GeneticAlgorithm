279627cdcd5fbc90fbb2562e78227904521e445f
Piotr Mularczyk <piotr.mularczyk8@gmail.com>
2021-04-03 18:01:28 +0200

    added elite strategy

--- src/GeneticAlgorithm.java
+++ src/GeneticAlgorithm.java
@@ -1,28 +1,30 @@
+import crossing.*;
 import mutation.*;
+import selection.*;
 import someMethods.FloatToBytes;
 import someMethods.SomeMethods;
-import crossing.*;
-import selection.*;
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class GeneticAlgorithm
-{
+public class GeneticAlgorithm {
     int populationAmount;
     List<Float> population;
     float epochsNumber;
     ISelection selection;
     ICrossover crossover;
     IMutation mutation;
-    FloatToBytes converter=new FloatToBytes(-10,10);
+    FloatToBytes converter = new FloatToBytes(-10, 10);
 
     public GeneticAlgorithm(int populationAmount, float epochsNumber, Selection selection,
                             Crossover crossover, int crossoverProbability,
-                            Mutation mutation, int mutationProbability)
-    {
-        this.populationAmount=populationAmount;
-        this.epochsNumber=epochsNumber;
+                            Mutation mutation, int mutationProbability) {
+        this.populationAmount = populationAmount;
+        this.epochsNumber = epochsNumber;
         generatePopulation();
         selectionChoice(selection);
         mutationChoice(mutation, mutationProbability);
@@ -30,115 +32,105 @@ public class GeneticAlgorithm
     }
 
 
-    public void run()
-    {
+    public void run() {
 
-        Random random=new Random();
-        for(int i=0;i<epochsNumber;i++)
-        {
-            System.out.println("Epoka: "+i);
-            System.out.println("Populacja: "+SomeMethods.get(population));
-            System.out.println("Najlepszy: "+getBest());
+        Random random = new Random();
+        for (int i = 0; i < epochsNumber; i++) {
+            //save best from population
+            Float best=getBest();
+
+            System.out.println("Epoka: " + i);
+            System.out.println("Populacja: " + SomeMethods.get(population));
+            System.out.println("Najlepszy: " + best);
             System.out.println("\n\n");
             //selection
-            population=selection.select(population);
+            population = selection.select(population);
 
             //crossing
-            List<Float> chosenOnesCopy=new ArrayList<>(population);
-            while(population.size()<populationAmount)
-            {
+            List<Float> chosenOnesCopy = new ArrayList<>(population);
+            while (population.size() < populationAmount-1) {
                 List<Integer> range = IntStream.range(0, chosenOnesCopy.size()).boxed()
                         .collect(Collectors.toCollection(ArrayList::new));
                 Collections.shuffle(range);
-                StringBuilder childOne= new StringBuilder(converter.floatToBinary(chosenOnesCopy.get(range.remove(0))));
-                StringBuilder childTwo= new StringBuilder(converter.floatToBinary(chosenOnesCopy.get(range.remove(0))));
+                StringBuilder childOne = new StringBuilder(converter.floatToBinary(chosenOnesCopy.get(range.remove(0))));
+                StringBuilder childTwo = new StringBuilder(converter.floatToBinary(chosenOnesCopy.get(range.remove(0))));
                 crossover.cross(
                         childOne,
                         childTwo
                 );
-                if(random.nextInt(2)==0) population.add(converter.binaryToFloat(childOne.toString()));
+                if (random.nextInt(2) == 0) population.add(converter.binaryToFloat(childOne.toString()));
                 else population.add(converter.binaryToFloat(childTwo.toString()));
             }
 
             //mutation
-            for(int j=0;j<population.size();j++)
-            {
-                float mutatingNumber=population.get(j);
-                StringBuilder mutatingNumberBinary=new StringBuilder(converter.floatToBinary(mutatingNumber));
+            for (int j = 0; j < population.size(); j++) {
+                float mutatingNumber = population.get(j);
+                StringBuilder mutatingNumberBinary = new StringBuilder(converter.floatToBinary(mutatingNumber));
                 mutation.mutate(mutatingNumberBinary);
-                population.set(j,converter.binaryToFloat(mutatingNumberBinary.toString()));
+                population.set(j, converter.binaryToFloat(mutatingNumberBinary.toString()));
             }
+            //add best to population
+            population.add(best);
         }
     }
 
-    void generatePopulation()
-    {
-        population=new ArrayList<>();
-        Random random=new Random();
-        for(int i=0;i<populationAmount;i++)
-        {
-            population.add(random.nextFloat()*5+5);
-            System.out.println("Added: "+population.get(population.size()-1));
+    void generatePopulation() {
+        population = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < populationAmount; i++) {
+            population.add(random.nextFloat() * 5 + 5);
+            System.out.println("Added: " + population.get(population.size() - 1));
         }
         SomeMethods.print(population);
     }
 
-    void selectionChoice(Selection selection)
-    {
-        switch (selection)
-        {
+    void selectionChoice(Selection selection) {
+        switch (selection) {
             case Best:
-                this.selection=new BestSelection(5);
+                this.selection = new BestSelection(5);
                 break;
             case Roulette:
-                this.selection=new RouletteSelection(5);
+                this.selection = new RouletteSelection(5);
                 break;
             case Tournament:
-                this.selection=new TournamentSelection(5);
+                this.selection = new TournamentSelection(5);
         }
     }
 
-    void crossoverChoice(Crossover crossover, int probability)
-    {
-        switch (crossover)
-        {
+    void crossoverChoice(Crossover crossover, int probability) {
+        switch (crossover) {
             case OnePoint:
-                this.crossover=new OnePointCrossing(5,probability,true);
+                this.crossover = new OnePointCrossing(5, probability, true);
                 break;
             case TwoPoint:
-                this.crossover=new TwoPointCrossing(5,10,probability,true);
+                this.crossover = new TwoPointCrossing(5, 10, probability, true);
                 break;
             case Homogeneours:
-                this.crossover=new HomogeneousCrossing(true, probability);
+                this.crossover = new HomogeneousCrossing(true, probability);
                 break;
         }
     }
 
 
-    void mutationChoice(Mutation mutation, int probability)
-    {
-        switch (mutation)
-        {
+    void mutationChoice(Mutation mutation, int probability) {
+        switch (mutation) {
             case OnePoint:
-                this.mutation=new OnePointMutation(5,probability);
+                this.mutation = new OnePointMutation(5, probability);
                 break;
             case TwoPoint:
-                this.mutation=new TwoPointMutation(5,20,probability);
+                this.mutation = new TwoPointMutation(5, 20, probability);
                 break;
             case Border:
-                this.mutation=new BorderMutation(true,probability);
+                this.mutation = new BorderMutation(true, probability);
                 break;
         }
     }
 
-    Float getBest()
-    {
-        Float min=Float.MAX_VALUE;
-        for(Float f : population)
-        {
-            if(SomeMethods.fun(f)<SomeMethods.fun(min))
-            {
-                min=f;
+    Float getBest() {
+        Float min = Float.MAX_VALUE;
+        for (Float f : population) {
+            if (SomeMethods.fun(f) < SomeMethods.fun(min)) {
+                min = f;
             }
         }
         return min;
