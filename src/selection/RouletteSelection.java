78e5695ea527a7fb5ad70599227b3f72a3342cc3
Piotr Mularczyk <piotr.mularczyk8@gmail.com>
2021-04-05 16:06:33 +0200

    fix RouletteSelection

--- src/selection/RouletteSelection.java
+++ src/selection/RouletteSelection.java
@@ -1,53 +1,52 @@
 package selection;
 
-import java.util.*;
 import someMethods.SomeMethods;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static java.lang.Math.abs;
 
 //wybor losowy. Im lepszy osobnik - tym ma wieksze szanse
 public class RouletteSelection implements ISelection {
     int chosenAmount;//ile osobnikow ma byc wybranych
-    public RouletteSelection (int chosenAmount)
-    {
-        this.chosenAmount=chosenAmount;
+
+    public RouletteSelection(int chosenAmount) {
+        this.chosenAmount = chosenAmount;
     }
 
     @Override
-    public List<Float> select(List<Float> population){
-        float sum=0;
-        for(Float f : population)
-        {
-            sum+= SomeMethods.fun(f);
-        }
-        List<Float> chosenOnes=new ArrayList<>();
-        for(int i=0;i<chosenAmount;i++)
-        {
+    public List<Float> select(List<Float> population) {
+        List<Float> chosenOnes = new ArrayList<>();
+        for (int i = 0; i < chosenAmount; i++) {
             chosenOnes.add(getOne(population));
         }
         return chosenOnes;
     }
 
-    public Float getOne(List<Float> population)
-    {
-        float sum=0;
-        for(Float f : population)
-        {
-            sum+= SomeMethods.fun(f);
+    public Float getOne(List<Float> population) {
+        float sum = 0;
+        for (Float f : population) {
+            sum += abs(SomeMethods.fun(f));
         }
 
-        Random random=new Random();
-        float rand=random.nextInt()%(int)sum;
-        int ind=0;
-        for(float i=0;i<sum;i+= SomeMethods.fun(population.get(ind)))
-        {
-            if(i>=rand) {
-                Float temp=population.get(ind);
+        Random random = new Random();
+        float rand = random.nextInt() % (int) sum;
+        int ind = 0;
+        for (float i = 0; i < sum; ) {
+            if (i >= rand) {
+                Float temp = population.get(ind);
                 population.remove(temp);
                 return temp;
             }
-            ind++;
+            if (ind < population.size()) {
+                i += abs(SomeMethods.fun(population.get(ind)));
+                ind++;
+            }
         }
 
-        float temp=population.get(ind);
+        float temp = population.get(ind-1);
         population.remove(temp);
         return temp;
     }
